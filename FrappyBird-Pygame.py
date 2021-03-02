5634e89af9274e8fde11a83bd4e263c984df27f5
GameTL <limsila.limsila@yahoo.com>
2021-03-02 20:46:26 +0700

    handing off to PC

--- FrappyBird-Pygame.py
+++ FrappyBird-Pygame.py
@@ -1,6 +1,23 @@
-# object are pipe, bird and ground
+
+# Making Frappy Bird with Pygame
+# Scorce https://www.youtube.com/watch?v=3ZPQI2-ciWI
 
 import pygame
-import neat
-import time
+import sys
 import random
+
+
+pygame.init()  # initialising the pygame, like main()
+
+
+screen = pygame.display.set_mode((576, 1024))  # Screen Resolution
+game_background = pygame.image.load("assets/picture/background-day.png").convert()
+game_background = pygame.transform.scale(game_background, (576, 1024))
+
+while True:
+    for event in pygame.event.get():  # the block for quitting and prevent initial lopping 
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
--- Main FrappyBird.py
+++ Main FrappyBird.py
@@ -5,9 +5,10 @@ import neat
 import time
 import random
 import os
+import sys
 
-resolution_width = 576  #inital = 576
-resolution_height = 1024  #inital = 1024
+resolution_width = 576  # inital = 576
+resolution_height = 1024  # inital = 1024
 
 '''
 game_background = pygame.image.load("assets/picture/background-day.png").convert()
@@ -22,7 +23,9 @@ pipe_imgs = pygame.transform.scale2x(pygame.image.load("assets/picture/pipe-gree
 base_imgs = pygame.transform.scale2x(pygame.image.load("assets/picture/base.png"))
 background_imgs = pygame.transform.scale2x(pygame.image.load("assets/picture/background-day.png"))
 
+
 class Bird():
+    imgs = bird_imgs
     MAX_ROTATION = 25 # how much tilt
     ROTATION_VELOCITY = 20 
     ANIMATION_TIME = 5 # how fast the bird flap its wiong
@@ -35,13 +38,13 @@ class Bird():
         self.velocity = 0
         self.height = self.y
         self.img_count = 0 # for keeping track of which image is showing
-    
+
     def jump(self): # flap up, for pygame (0,0) is the top left of the window
         # negetive y is up and positive y is down, x direction is conventional
         self.velocity = -10.5 # jump up with a velocity of 10.5
         self.tick_count = 0
         self.height = self.y
-    
+
     def move(self):
         self.tick_count += 1 # pretty much a framerate counter
 
@@ -51,5 +54,60 @@ class Bird():
         if displacement < 0:
             displacement -= 2
 
+        self.y = self.y + displacement
+
+        if displacement < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROTATION_VELOCITY
+
+    def draw(self, window):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.imgs = self.bird_imgs[0] ####
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.imgs = self.bird_imgs[1] ####
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.imgs = self.bird_imgs[2] ####
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.imgs = self.bird_imgs[1] ####
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.imgs = self.bird_imgs[0] ####
+            self.img_count = 0
+        if self.tilt <= -80:
+            self.imgs = self.bird_imgs[1]
+            self.img_count = self.ANIMATION_TIME
+
+        # this is for rotating the bird up and down with the centre of the image as a pivot
+        rotated_image = pygame.transform.rotate(self.imgs, self.tilt)
+        new_rect = rotated_image.get_rect(centre=self.bird_imgs.get_rect(topleft = (self.x,self.y)).center)
+        window.blit(rotated_image, new_rect.top.left)
+
+    def get_mask(self):
+        return pygame.masl.from_surface(self.imgs)
 
 
+def draw_window(window, Bird):
+    window.blit(background_imgs, (0, 0))
+
+    Bird.draw(window)
+    pygame.display.update()
+
+
+def main():
+    bird = Bird(200, 200)  # position 200
+    window = pygame.display.set_mode((resolution_width, resolution_height))
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        draw_window(window, bird)
+        pygame.display.update()
+
+
+main()
